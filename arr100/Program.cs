9f59af77c6b31405e79affe8a222a72679ba32da
Alexandr <sasha732sasha000@gmail.com>
2017-12-03 10:14:16 +0200

    hochu zdohnut'

--- arr100/Program.cs
+++ arr100/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
 
 namespace arr100
 {
@@ -6,7 +8,50 @@ namespace arr100
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new[]{1,1,1,3,3,3,4,5,5,9};
+            List<int> res;
+            Solution.RemoveDuplicatesElements(arr, out res);
+
+            foreach (int element in res)
+            {
+                Console.Write(element + " ");
+            }
+
+            Console.ReadKey();
         }
+    }
+
+    static class Solution
+    {
+
+
+        public static void RemoveDuplicatesElements(int[] array, out List<int> result)
+        {
+            result = new List<int>(array);
+
+            for (int i = 0; i < result.Count - 1; i++)
+            {
+                int k = 0;
+                int countOfRepeats = 0;
+
+                for (int j = 0; j < result.Count; j++)
+                {
+                    if (result[i] == result[j])
+                    {
+                        k = j;
+                        countOfRepeats++;
+                    }
+                }
+                if (countOfRepeats == 2)
+                {
+                    result.RemoveAt(k);
+                    result.RemoveAt(i);
+                    
+                    i = 0;
+                }
+            }
+
+        }
+
     }
 }
---
+++ arr116/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace arr116
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1, 1, 1, 2, 2, 1, 1, 3, 3, 3, 4};
+            List<int> nOS;
+            List<int> eOS;
+
+            Solution.GetElementsOfSeries(array, out nOS, out eOS);
+
+            foreach (int element in eOS)
+               Console.Write(element + " ");
+
+            Console.WriteLine();
+
+            foreach (int num in nOS)
+                Console.Write(num + " ");
+
+            Console.ReadKey();
+        }
+    }
+
+    static class Solution
+    {
+        public static void GetElementsOfSeries(int[] array, out List<int> numOfSeries, out List<int> elementsOfSeries)
+        {
+            numOfSeries = new List<int>();
+            elementsOfSeries = new List<int>();                     // po zhelaniyu mozhno sdelat' s etih spiskov massivi kak v uslovii no ya dumayu v etom net neobhodimosti
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                elementsOfSeries.Add(array[i]);
+                int k = i + 1;
+                int m = i;
+                while (k < array.Length && array[i] == array[k])
+                {
+                    k++;
+                    i++;
+                }
+                numOfSeries.Add(k - m);
+            }
+
+            
+        }
+    }
+}
